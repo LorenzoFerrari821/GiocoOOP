944cf51753aeb9db564c9722666e49288570a131
Lykens <lollone96@hotmail.com>
2017-04-13 23:05:33 +0200

    Aggiunta file audio

--- src/gioco/Gioco.java
+++ src/gioco/Gioco.java
@@ -1,6 +1,7 @@
 package gioco;
 
 import java.awt.EventQueue;
+import java.io.File;
 
 /**
  * Classe principale che dirige il funzionamento del gioco dall'esterno.
@@ -8,13 +9,15 @@ import java.awt.EventQueue;
  * @version 1.0
  */
 public class Gioco {
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUIMenuPrincipale frame = new GUIMenuPrincipale();
 					frame.setVisible(true);
+					File musica= new File("media/Musica.wav");
+					MenuMusic.PlaySound(musica);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
