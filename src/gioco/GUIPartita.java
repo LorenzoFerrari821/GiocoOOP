ee8c11ac38b95b54894a9798280325884fcc348a
Werther <werther.berselli@gmail.com>
2017-04-17 18:06:10 +0200

    Portata avanti classe Partita, ma ancora da terminare

--- src/gioco/GUIPartita.java
+++ src/gioco/GUIPartita.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -19,6 +20,7 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
@@ -28,8 +30,8 @@ public class GUIPartita extends JFrame{
 	private JPanel contentPane;
 	private JPanel panelTop;
 	private JPanel panelBottom;
-	private JPanel panelSx;
-	private JPanel panelDx;
+	private JPanel panelCenter;
+	private JPanel panelBtnGoRight;
 	private JPanel panelPartita;
 	private JPanel panelStats;
 	private JPanel panelMappa;
@@ -40,11 +42,13 @@ public class GUIPartita extends JFrame{
 	private JLabel lblMaterialiv;
 	private JLabel lblPuntiRicerca;
 	private JLabel lblPuntiRicercav;
-	private JButton btnGoUp;
-	private JButton btnGoRight;
-	private JButton btnGoDown;
-	private JButton btnGoLeft;
+	private RoundedCornerButton btnGoUp;
+	private RoundedCornerButton btnGoRight;
+	private RoundedCornerButton btnGoDown;
+	private RoundedCornerButton btnGoLeft;
 	private JPanel panelBtnGoUp;
+	private JPanel panelBtnGoDown;
+	private JPanel panelBtnGoLeft;
 	
 	GUIPartita()
 	{
@@ -70,12 +74,14 @@ public class GUIPartita extends JFrame{
 			e1.printStackTrace();
 		}
 		
-		contentPane = new JPanel(new BorderLayout(0, 0));
+		contentPane = new JPanel();
+		contentPane.setLayout(new GridLayout2(3, 1, 0, 0));
 		contentPane.setBorder(new EmptyBorder(0 , 0 , 0 , 0 ));
 		panelTop = new JPanel(new GridLayout(1, 3, 0, 0));
-		panelBottom = new JPanel(new BorderLayout(0, 0));
-		panelSx = new JPanel(new BorderLayout (0, 0));
-		panelDx = new JPanel(new BorderLayout(0, 0));
+		panelBottom = new JPanel(new GridLayout(1, 3, 0, 0));
+		panelCenter = new JPanel(new GridLayout(1, 3, 0, 0));
+		panelBtnGoRight = new JPanel();
+		panelBtnGoLeft = new JPanel();
 		panelTop.setBackground(Color.DARK_GRAY);
 		
 		lblOro = new JLabel("Oro: ");
@@ -90,24 +96,35 @@ public class GUIPartita extends JFrame{
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
+		c.anchor = GridBagConstraints.WEST;
 		panelStats.add(lblOro, c);
+		c.gridx ++;
+		panelStats.add(new JLabel("  "), c);
 		c.gridx ++;
 		panelStats.add(lblOrov, c);
 		c.gridx ++;
+		panelStats.add(new JLabel("      "), c);
+		c.gridx ++;
 		panelStats.add(lblMateriali, c);
+		c.gridx ++;
+		panelStats.add(new JLabel("  "), c);
 		c.gridx ++;
 		panelStats.add(lblMaterialiv, c);
 		c.gridx ++;
+		panelStats.add(new JLabel("      "), c);
+		c.gridx ++;
 		panelStats.add(lblPuntiRicerca, c);
 		c.gridx ++;
+		panelStats.add(new JLabel("  "), c);
+		c.gridx ++;
 		panelStats.add(lblPuntiRicercav, c);
-
+		
 		panelTop.add(panelStats);
 		
-		btnGoUp = new JButton();
-		btnGoRight = new JButton("DESTRA");
-		btnGoDown = new JButton("GIU");
-		btnGoLeft = new JButton("SINISTRA");
+		btnGoUp = new RoundedCornerButton();
+		btnGoRight = new RoundedCornerButton();
+		btnGoDown = new RoundedCornerButton();
+		btnGoLeft = new RoundedCornerButton();
 		
 		ImageIcon iconbtnGoUp = new ImageIcon("media/btnGoUp.png");
 		Image scalebtnGoUp = iconbtnGoUp.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
@@ -121,9 +138,64 @@ public class GUIPartita extends JFrame{
 		panelBtnGoUp.add(btnGoUp);
 		
 		panelTop.add(panelBtnGoUp);
-		panelTop.add(new JLabel("Ciao mondo"));
+		JLabel mondo = new JLabel("Ciao Mondo");
+		mondo.setHorizontalAlignment(SwingConstants.RIGHT);
+		panelTop.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		panelTop.add(mondo);
 		
-		contentPane.add(panelTop, BorderLayout.NORTH);
+		btnGoRight = new RoundedCornerButton();
+		ImageIcon iconbtnGoRight = new ImageIcon("media/btnGoRight.png");
+		Image scalebtnGoRight = iconbtnGoUp.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+		btnGoRight.setIcon(iconbtnGoRight);
+		btnGoRight.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+
+		panelBtnGoRight.setBackground(Color.BLUE);
+		panelBtnGoRight.add(btnGoRight);
+		
+		btnGoLeft = new RoundedCornerButton();
+		ImageIcon iconbtnGoLeft = new ImageIcon("media/btnGoLeft.png");
+		Image scalebtnGoLeft = iconbtnGoLeft.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+		btnGoLeft.setIcon(iconbtnGoLeft);
+		btnGoLeft.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+
+		panelBtnGoLeft.setBackground(Color.BLUE);
+		panelBtnGoLeft.add(btnGoLeft);
+		
+		panelCenter.add(panelBtnGoLeft);
+		panelCenter.add(new JLabel("ciao"));
+		panelCenter.add(panelBtnGoRight);
+		
+		panelBottom.add(new JLabel("Ciao mondo"));
+		
+		btnGoDown = new RoundedCornerButton();
+		ImageIcon iconbtnGoDown = new ImageIcon("media/btnGoDown.png");
+		Image scalebtnGoDown = iconbtnGoDown.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+		btnGoDown.setIcon(iconbtnGoDown);
+		btnGoDown.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+		
+		panelBtnGoDown = new JPanel();
+		panelBtnGoDown.setBackground(Color.BLUE);
+		panelBtnGoDown.add(btnGoDown);
+		
+		panelBottom.add(panelBtnGoDown);
+		
+		panelBottom.add(new JLabel("Ciao mondo"));
+		
+		contentPane.add(panelTop);
+		contentPane.add(panelCenter);
+		contentPane.add(panelBottom);
 		this.add(contentPane);
 	}
 }
